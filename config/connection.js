547f0b7dca9f05aaa09c2d3e0b5aab82bed207da
John Krug <john@Johns-MacBook-Pro.local>
2017-03-25 10:55:26 -0400

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var app = require('../server');
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burger",
-  // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
-});
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      port: 3306,
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "burger",
+    });
+  };
 
 
 // Make connection.
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var methodOverride = require("method-override");
 
 var port = process.env.PORT || 3000;
 
-var app = express();
+var app = module.exports() = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
